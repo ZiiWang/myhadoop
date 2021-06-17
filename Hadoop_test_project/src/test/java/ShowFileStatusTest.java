1ff2296647c0b527cc8bc0f473a4425a892d3ef7
ZiiWang <zwan411@gmail.com>
2021-06-17 11:44:31 +0800

    change the structure of the project, use maven to replace local dependency

--- Hadoop_test_project/src/test/java/ShowFileStatusTest.java
+++ Hadoop_test_project/src/test/java/ShowFileStatusTest.java
@@ -4,7 +4,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.hdfs.MiniDFSCluster;
 
 import java.io.IOException;
 import java.io.OutputStream;
